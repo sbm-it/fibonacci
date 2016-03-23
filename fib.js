7dbf626ed11ef7e6e47077f1e70ee380150fa621
Jonas Almeida <jalmeida@mathbiol.org>
2016-03-23 16:22:52 -0400

    fib generator

--- fib.js
+++ fib.js
@@ -1,2 +1,36 @@
 console.log('fib.js loaded')
-sbmApps.render('<h3 style="color:red">lala Fibonacci</h3>')
+
+false||(function(){ // wrapping everything within an annonymous function
+    var h = '<h3 style="color:navy">Fibonacci generator</h3>'
+    h +='<button id="fibButton" type="button" class="btn btn-primary">Generate</button> '
+    h +='Lenght: <input id="fibLength" size=4 value="20"> '
+    h +='Seed: <input id="fibSeed" size=5 value="0,1"> '
+    h +='<p id="fibArray" style="color:navy"></p>'
+    sbmApps.render(h)
+    // style
+    fibSeed.style.color="blue"
+    fibSeed.style.border=0
+    fibLength.style.color="blue"
+    fibLength.style.border=0
+    // fib generator
+    var fib = function(n,x){
+        if(!n){n=10}
+        if(!x){x= [0,1]}
+        x.push(parseInt(x.slice(-1))+parseInt(x.slice(-2,-1)))
+        if(x.length>n){
+            return x
+        }else{
+            return fib(n,x)
+        }
+    }
+    // generate
+    fibButton.onclick=function(){
+        var x = fib(parseInt(fibLength.value),JSON.parse('['+fibSeed.value+']'))
+        fibArray.textContent=x.join(', ')
+        fibSeed.value=x.slice(-2) // reseed
+        fibSeed.size=fibSeed.value.length+5 // adjust size of seed input element
+    }
+
+    4
+})()
+
